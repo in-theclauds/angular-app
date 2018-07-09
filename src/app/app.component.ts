93bc80de508ea97a2ad6fb665f013ea93d50ec87
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-07-08 23:45:41 -0400

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  title = 'my test app!!';
+  animals: Array<Object> = [
+    {
+      id: 1,
+      category: 'mammal',
+      name: 'Dog'
+    }, {
+      id: 2,
+      category: 'oviparous',
+      name: 'Duck'
+    }, {
+      id: 3,
+      category: 'mammal',
+      name: 'Elephant'
+    }, {
+      id: 4,
+      category: 'reptile',
+      name: 'Snake'
+    }
+  ];
 }
